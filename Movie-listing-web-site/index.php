8019b05ece449e3d08475f54d58e96a6032e04e4
Justin Diaz <38015681+justinok@users.noreply.github.com>
2022-09-27 14:15:11 -0500

    on the go

--- Movie-listing-web-site/index.php
+++ Movie-listing-web-site/index.php
@@ -3,9 +3,9 @@
 <div class="col-md-3 ">
     
 <div class="card">
-    <img class="card-img-top" src="https://www.pngall.com/wp-content/uploads/13/Movie-PNG-Images-HD.png" alt="">
+    <img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg" alt="">
     <div class="card-body">
-        <h4 class="card-title">Title</h4>
+        <h4 class="card-title">The Avengers</h4>
         <a name="" id="" class="btn btn-primary" href="#" role="button"> ver mas</a>
     </div>
 </div>
@@ -15,9 +15,9 @@
 <div class="col-md-3 ">
     
 <div class="card">
-    <img class="card-img-top" src="https://www.pngall.com/wp-content/uploads/13/Movie-PNG-Images-HD.png" alt="">
+    <img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg" alt="">
     <div class="card-body">
-        <h4 class="card-title">Title</h4>
+        <h4 class="card-title">Avenger: Endgame</h4>
         <a name="" id="" class="btn btn-primary" href="#" role="button"> ver mas</a>
     </div>
 </div>
@@ -28,9 +28,9 @@
 <div class="col-md-3 ">
     
 <div class="card">
-    <img class="card-img-top" src="https://www.pngall.com/wp-content/uploads/13/Movie-PNG-Images-HD.png" alt="">
+    <img class="card-img-top" src="https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg" alt="">
     <div class="card-body">
-        <h4 class="card-title">Title</h4>
+        <h4 class="card-title">Avenger: Infinity War</h4>
         <a name="" id="" class="btn btn-primary" href="#" role="button"> ver mas</a>
     </div>
 </div>
